f88750aee6b66e86733553c9dc93f5fb378e2ec9
amy <amy.orquidea57@gmail.com>
2025-08-12 08:40:36 -0300

    remoção da logo e do hexagono

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+document.getElementById('botao1').addEventListener('click', function() {
+    window.location.href = '/';
+});
+
+document.getElementById('botao2').addEventListener('click', function() {
+    window.location.href = '/';
+});
+
+document.getElementById('botao3').addEventListener('click', function() {
+    window.location.href = '/';
+});
