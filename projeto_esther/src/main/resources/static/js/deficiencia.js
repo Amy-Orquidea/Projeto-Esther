9df22ef079ad0822353218bc830e32fd7ef53408
Amy <victorhugobaiajunqueira@gmail.com>
2025-08-16 17:20:41 -0300

    adição do javascript da aba de deficiencia, juntamente da sua header com botões

---
+++ projeto_esther/src/main/resources/static/js/deficiencia.js
@@ -0,0 +1,23 @@
+// 1. Encontre todos os elementos com a classe 'botao-muda-cor'
+const botoes = document.getElementsByClassName('botao-header');
+
+// 2. Iterar sobre a coleção de botões
+// O loop 'for...of' é uma forma moderna e fácil de fazer isso.
+for (const botao of botoes) {
+    botao.addEventListener('click', function() {
+            const corOriginal = this.style.color;
+            this.style.color = '#fde070'; // Muda a cor do texto para amarelo
+                setTimeout(() => {
+            this.style.color = corOriginal;
+        }, 300);
+        });
+    document.getElementById('esquerda').addEventListener('click', function() {
+        window.location.href = 'y';
+    });
+    document.getElementById('meio').addEventListener('click', function() {      
+        window.location.href = 'x';
+    });
+    document.getElementById('direita').addEventListener('click', function() {
+        window.location.href = 'z';
+    });
+}
